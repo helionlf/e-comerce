ffbf32e9c94750d1cc2ee361f5725b7ed7b33ed2
Hélio Nogueira <heliofilhofhnlf@gmail.com>
2024-12-30 17:23:58 -0300

    tabela categoria criada

--- database/migrations/2024_12_30_194945_create_categorias_table.php
+++ database/migrations/2024_12_30_194945_create_categorias_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('produtos', function (Blueprint $table) {
+        Schema::create('categorias', function (Blueprint $table) {
             $table->id();
-            $table->string('nome');
+            $table->text('nome');
             $table->text('descricao');
-            $table->decimal('valor');
-            $table->string('slug');
-            $table->string('imagem');
-            $table->integer('estoque');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('produtos');
+        Schema::dropIfExists('categorias');
     }
 };
